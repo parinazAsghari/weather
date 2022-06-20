84648c630095d153dedc7471842506557323c768
p.asghari <parinazasghari75@gmail.com>
2022-06-20 08:55:57 +0430

    service.g

---
+++ lib/models/service.g.dart
@@ -0,0 +1,52 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'service.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ServiceAdapter extends TypeAdapter<Service> {
+  @override
+  final int typeId = 3;
+
+  @override
+  Service read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Service(
+      id: fields[0] as int?,
+      serviceName: fields[1] as String?,
+      iconPath: fields[2] as String?,
+      isFavorite: fields[3] as bool?,
+      isFixed: fields[4] as bool?,
+      slideId: fields[5] as int?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Service obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.serviceName)
+      ..writeByte(2)
+      ..write(obj.iconPath)
+      ..writeByte(3)
+      ..write(obj.isFavorite)
+      ..writeByte(4)
+      ..write(obj.isFixed)
+      ..writeByte(5)
+      ..write(obj.slideId);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) => identical(this, other) || other is ServiceAdapter && runtimeType == other.runtimeType && typeId == other.typeId;
+}
